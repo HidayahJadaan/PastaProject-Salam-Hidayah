b3c35a70c7d7010e292196150419138630a267a9
Hidayah Al-Jadaan <hkaljadaan18@cit.just.edu.jo>
2025-04-16 13:37:42 +0300

    Finishing creating chef and customer models

---
+++ src/app/features/customer/customers-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersModule } from './users.module';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { UsersFormComponent } from './components/users-form/users-form.component';
+import { UsersDetailsComponent } from './components/users-details/users-details.component';
+import { CanActivateUserType } from '../../guards/user-type.guard';
+import { checkUserType } from './guards/user-type-fn.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'list',
+  },
+  {
+    path: 'list',
+    component: UsersListComponent,
+  },
+  {
+    path: 'add',
+    component: UsersFormComponent,
+    // canActivate: [CanActivateUserType],
+    canActivate:[checkUserType]
+    // canActivate: [checkUserTypeFn]
+  },
+  {
+    path: 'edit/:id',
+    component: UsersFormComponent,
+    // canActivate: [CanActivateUserType],
+    canActivate:[checkUserType]
+  },
+  {
+    path: 'details/:id',
+    component: UsersDetailsComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsersRoutingModule { }
